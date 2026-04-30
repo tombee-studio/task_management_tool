243f55fed50418f54e0f3d053f1d0ea62dc07c65
とんび（Tombee） <admin@tombee-studio.com>
2026-04-30 22:46:03 +0900

    #48 親チケットを表示するように修正

--- task_app/views.py
+++ task_app/views.py
@@ -203,8 +203,17 @@ class TaskDetailView(LoginRequiredMixin, DetailView):
         selected_status_list = self.request.GET.getlist("status")
         context["selected_status_list"] = list(map(lambda x: int(x), selected_status_list))
         context["status_list"] = Status.objects.all()
+        context["status_filter"] = "&".join(list(map(lambda status: f"status={status}", selected_status_list)))
         context["tasks"] = self.object.tasks.filter(
             status__in=selected_status_list)
+
+        parent_objects = []
+        current = self.object
+        while current != None:
+            parent_objects.insert(0, current)
+            current = current.parent
+        context["parent_objects"] = parent_objects
+        
         return context
 
 
