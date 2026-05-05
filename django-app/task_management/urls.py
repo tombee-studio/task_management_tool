4f1e55b6b16eb862ee776c36dd3a94ae8b78762d
とんび（Tombee） <admin@tombee-studio.com>
2026-05-06 02:47:27 +0900

    #1 '/' のURLにアクセスしたとき、プロジェクト一覧画面に遷移するように修正

--- django-app/task_management/urls.py
+++ django-app/task_management/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='project_list', permanent=False)),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('task_app/', include('task_app.urls')),
