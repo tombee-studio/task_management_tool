a354930259a2ae3f8fdd967d375a818226e94078
とんび（Tombee） <admin@tombee-studio.com>
2026-06-15 10:50:19 +0900

    Ftr: Send SQS on assignee->Automation change #185
    - Add pre_save to capture previous assignee_id
    - Trigger SQS only when assignee set to Automation
    - Include reporter_id in SQS message body
    
    Task: [185](https://8hzryl7735.execute-api.ap-northeast-1.amazonaws.com/prd/task_app/tasks/185)

--- django-app/task_app/signals.py
+++ django-app/task_app/signals.py
@@ -1,7 +1,7 @@
 import json
 import os
 import secrets
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.utils import timezone
@@ -9,14 +9,17 @@ from .models import Task, Comment, UserPreferences
 from .rules import process_task_rules
 
 
-def _send_task_to_sqs(task_id):
+def _send_task_to_sqs(task_id, reporter_id=None):
     queue_url = os.environ.get("SQS_QUEUE_URL", "")
     if not queue_url:
         return
     import boto3
+    body = {"task_id": task_id}
+    if reporter_id:
+        body["reporter_id"] = reporter_id
     boto3.client("sqs").send_message(
         QueueUrl=queue_url,
-        MessageBody=json.dumps({"task_id": task_id}),
+        MessageBody=json.dumps(body),
     )
 
 
@@ -34,15 +37,31 @@ def create_user_preferences(sender, instance, created, **kwargs):
         )
 
 
+@receiver(pre_save, sender=Task)
+def task_pre_save(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            instance._prev_assignee_id = Task.objects.get(pk=instance.pk).assignee_id
+        except Task.DoesNotExist:
+            instance._prev_assignee_id = None
+    else:
+        instance._prev_assignee_id = None
+
+
 @receiver(post_save, sender=Task)
 def task_saved(sender, instance, created, **kwargs):
     try:
         process_task_rules(instance)
     except Exception:
         pass
-    if created:
+    if not created:
         try:
-            _send_task_to_sqs(instance.pk)
+            automation = User.objects.filter(username='Automation').first()
+            prev_id = getattr(instance, '_prev_assignee_id', None)
+            if (automation
+                    and instance.assignee_id == automation.pk
+                    and prev_id != automation.pk):
+                _send_task_to_sqs(instance.pk, reporter_id=prev_id)
         except Exception:
             pass
 
